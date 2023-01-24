7efb00a62a90ce12c55ed5bc45bbf2010349c804
Jeremy Colin <jeremypa.colin@gmail.com>
2023-01-24 23:41:08 +0100

    Add pages navigation

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,19 +1,31 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useParams, useNavigate } from "react-router-dom";
+import { useScrollToTop } from "../utils/use-scroll-to-top";
 import classes from "./page.module.css";
 
 export interface PageLoaderData {
   text: string;
-  next: string;
+  next: [{ label: string; page: number }];
 }
 
 export const Page = (props: any) => {
+  useScrollToTop();
   const { text, next } = useLoaderData() as PageLoaderData;
   const { page } = useParams();
+
+  const navigate = useNavigate();
 
   return (
     <div className={classes.page}>
       <p className={classes.title}>{page}</p>
       <p className={classes.text}>{text}</p>
+      {next.map((action) => (
+        <div
+          className={classes.button}
+          onClick={() => navigate(`/book/${action.page}`)}
+        >
+          {action.label}
+        </div>
+      ))}
     </div>
   );
 };
